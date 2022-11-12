1bceb7aa875c3797cbaa6e0ff03bcc6e568ab3e4
arad <aradbm@gmail.com>
2022-11-12 13:13:39 +0200

    main changes

--- main.c
+++ main.c
@@ -1,6 +1,39 @@
 #include <stdio.h>
 
 int main(){
-	printf("hello \n");
-	return 0;
+	int a;
+	int b; 
+	printf("enter first number:\n");
+	scanf("%d",&a);
+
+	printf("enter second number:\n");
+	scanf("%d",&b);
+	
+	printf("\nPrime numbers betweern a and b:");
+	// run from a to b on all numbers and check all functions
+	for(i=a;i<=b; i++)
+	{
+		if(isPrime(i))
+		printf(i);
+	}
+	printf("\nArmstrong numbers betweern a and b:");
+	for(i=a;i<=b; i++)
+	{
+		if(isArmstrong(i))
+		printf(i);
+	}
+	printf("\nStrong numbers betweern a and b:");
+	for(i=a;i<=b; i++)
+	{
+		if(isStrong(i))
+		printf(i);
+	}
+	printf("\nPalindrome numbers betweern a and b:");
+	for(i=a;i<=b; i++)
+	{
+		if(isPalindrome(i))
+		printf(i);
+	}
+	printf("\n");
+    return 0;
 }
