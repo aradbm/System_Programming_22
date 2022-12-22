f5aec7f535c8189d0b52ffc5b9f2f7d0fec73506
arad <aradbm@gmail.com>
2022-12-22 17:14:15 +0200

    isort finished with comments

--- isort.c
+++ isort.c
@@ -1,41 +1,50 @@
 #include <stdio.h>
 #define SIZE 50
-void shift_element(int *arr, int i)
+// printing the array
+void printarr(int *arr, int l)
 {
-    int temp = *(arr + i);
-    for (int j = i; j > 0; j--)
+    for (int i = 0; i < l; i++)
     {
-        *(arr + j) = *(arr + j - 1);
-    }
-}
-void insertion_sort(int *arr, int len)
-{
-    for (int i = 1; i < len; i++)
-    {
-        int j = i;
-        while (j > 0 && *(arr + j) < *(arr + j - 1))
-        {
-            shift_element(arr, j);
-            j--;
-        }
-    }
-}
-int main()
-{
-    int arr[50] = {0};
-    printf("Enter 50 array elements: ");
-    for (int i = 0; i < SIZE; i++)
-    {
-        scanf("%d", *(arr + i));
-    }
-    insertion_sort(arr, SIZE);
-    printf("Sorted array: ");
-    for (int i = 0; i < SIZE; i++)
-    {
-        if (i == SIZE - 1)
+        if (i == l - 1)
             printf("%d\n", *(arr + i));
         else
             printf("%d,", *(arr + i));
     }
+}
+// shifting the elements to the right by one and taking the last element and putting it in the first place
+void shift_element(int *arr, int i)
+{
+    int temp = *(arr + i);
+    for (int k = 0; k < i; k++)
+    {
+        *(arr + i - k) = *(arr + i - k - 1);
+    }
+    *(arr) = temp;
+}
+// sorting the array using insertion sort
+void insertion_sort(int *arr, int l)
+{
+    for (int i = 1; i < l; i++)
+    {
+        int j = i;
+        int count = 0;
+        while ((*(arr + i) < *(arr + j - 1)) && (j > 0))
+        {
+            j--;
+            count++;
+        }
+        if (i != j)
+            shift_element((arr + j), count);
+    }
+}
+int main()
+{
+    int arr[SIZE];
+    for (int i = 0; i < SIZE; i++)
+    {
+        scanf("%d", (arr + i));
+    }
+    insertion_sort(arr, SIZE);
+    printarr(arr, SIZE);
     return 0;
 }
